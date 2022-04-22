9333c0b78cc4d1347b3f472eb89472f02ef3c568
michaelcjoseph <michael.joseph@hey.com>
2022-04-22 10:06:26 -0500

    Set up helper function to create contract SVG

--- util/image.js
+++ util/image.js
@@ -1,7 +1,8 @@
 function getImage(identity, equipment, baseStats, bonusStats) {
+  // This should return a Base64 encoded string where the start of the string is `data:image/svg+xml;base64,`
   const startSvg = `<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 1200 650">`
-  const closeSvg = "</svg>"
   const backgroundRect = `<rect width="100%" height="100%" fill="#002B36" />`
+  const closeSvg = "</svg>"
   return startSvg + backgroundRect + closeSvg
 }
 
