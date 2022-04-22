be8829b56509982a321924a4f6a941fc7125eb0a
michaelcjoseph <michael.joseph@hey.com>
2022-04-22 18:56:48 -0500

    Set up functions to calculate stats bonus from identity

--- util/stats.js
+++ util/stats.js
@@ -1,5 +1,11 @@
 // Helper functions to populate Stats fields in Meta ID
 
+import {
+  getRace,
+  getRole,
+  getElement
+} from "./identity"
+
 function getEquipmentBonus(item) {
   if (item) {
     if (item.includes("of")) {
@@ -16,6 +22,58 @@ function getEquipmentBonus(item) {
   return 0
 }
 
+function getRaceBonus(race) {
+  return {
+    str: 0,
+    dex: 0,
+    con: 0,
+    int: 0,
+    wis: 0,
+    cha: 0
+  }
+}
+
+function getRoleBonus(role) {
+  return {
+    str: 0,
+    dex: 0,
+    con: 0,
+    int: 0,
+    wis: 0,
+    cha: 0
+  }
+}
+
+function getElementBonus(element) {
+  return {
+    str: 0,
+    dex: 0,
+    con: 0,
+    int: 0,
+    wis: 0,
+    cha: 0
+  }
+}
+
+function getIdentityBonus(identity) {
+  const race = getRace(identity)
+  const role = getRole(identity)
+  const element = getElement(identity)
+
+  const raceBonus = getRaceBonus(race)
+  const roleBonus = getRoleBonus(role)
+  const elementBonus = getElementBonus(element)
+
+  return {
+    str: raceBonus.str + roleBonus.str + elementBonus.str,
+    dex: raceBonus.dex + roleBonus.dex + elementBonus.dex,
+    con: raceBonus.con + roleBonus.con + elementBonus.con,
+    int: raceBonus.int + roleBonus.int + elementBonus.int,
+    wis: raceBonus.wis + roleBonus.wis + elementBonus.wis,
+    cha: raceBonus.cha + roleBonus.cha + elementBonus.cha
+  }
+}
+
 function getBonusStats(identity, equipment) {
   const { 
     weapon,
@@ -28,13 +86,15 @@ function getBonusStats(identity, equipment) {
     ring
   } = equipment
 
+  const idBonus = getIdentityBonus(identity)
+
   return {
-    str: getEquipmentBonus(weapon),
-    dex: getEquipmentBonus(footArmor) + getEquipmentBonus(handAmor),
-    con: getEquipmentBonus(chestArmor),
-    int: getEquipmentBonus(headArmor),
-    wis: getEquipmentBonus(waistArmor),
-    cha: getEquipmentBonus(necklace) + getEquipmentBonus(ring),
+    str: getEquipmentBonus(weapon) + idBonus.str,
+    dex: getEquipmentBonus(footArmor) + getEquipmentBonus(handAmor) + idBonus.dex,
+    con: getEquipmentBonus(chestArmor) + idBonus.con,
+    int: getEquipmentBonus(headArmor) + idBonus.int,
+    wis: getEquipmentBonus(waistArmor) + idBonus.wis,
+    cha: getEquipmentBonus(necklace) + getEquipmentBonus(ring) + idBonus.cha,
   }
 }
 
