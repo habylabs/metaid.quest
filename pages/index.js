5823c295b631b244d0f8c0c113feb61deb4adf6e
michaelcjoseph <michael.joseph@hey.com>
2022-06-10 10:52:40 -0500

    Clean up files

--- pages/index.js
+++ pages/index.js
@@ -1,143 +1,11 @@
-import { useEffect, useState } from 'react'
 import { useMediaQuery } from 'react-responsive'
-import { useWeb3React } from "@web3-react/core"
 import Head from 'next/head'
 import Image from 'next/image'
-import { Connect, MetaId } from '../components'
-import { getContractSvg } from '../util/image'
 import exampleGif from '../public/metaid-examples.gif'
-import styles from '../styles/components/Stats.module.css'
-
-function readStats({ address }) {
-  const { LVL, HP, MP, STR, DEX, CON, INT, WIS, CHA } = address
-  return (
-    <>
-      <p>Level: {LVL}</p>
-      <p>HP: {HP}</p>
-      <p>MP: {MP}</p>
-      <p>STR: {STR}</p>
-      <p>DEX: {DEX}</p>
-      <p>CON: {CON}</p>
-      <p>INT: {INT}</p>
-      <p>WIS: {WIS}</p>
-      <p>CHA: {CHA}</p>
-    </>
-  )
-}
+import styles from '../styles/pages/index.module.css'
 
 function Home() {
-  const { active, account } = useWeb3React()
   const isMobile = useMediaQuery({ maxWidth: 480 })
-  
-  const [hasId, setHasId] = useState(false)
-  const [isEligible, setIsEligible] = useState(false)
-
-  const db = {
-    identity: {
-      name: null,
-      pfp: {
-        contract: null,
-        id: null,
-        image: null,
-        race: null,
-        role: null,
-        element: null
-      },
-      character: {
-        id: null,
-        race: null,
-        role: null,
-        element: null
-      },
-    },
-    equipment: {
-      weapon: null,
-      chestArmor: null,
-      headArmor: null,
-      waistArmor: null,
-      footArmor: null,
-      handAmor: null,
-      necklace: null,
-      ring: null
-    },
-    baseStats: {
-      str: null,
-      dex: null,
-      con: null,
-      int: null,
-      wis: null,
-      cha: null,
-    },
-    bonusStats: {
-      str: null,
-      dex: null,
-      con: null,
-      int: null,
-      wis: null,
-      cha: null,
-    }
-  }
-
-  /*
-  const [data, setData] = useState(null)
-  const [isLoading, setLoading] = useState(true)
-
-  useEffect(() => {
-    setLoading(true)
-    fetch(`api/v1/stats/${account}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data)
-        setLoading(false)
-      })
-  }, [account])
-
-  if (isLoading) return <p>Loading...</p>
-
-  const dataComponent = (
-    <>
-      {isLoading ? <p>Loading...</p> : readStats(data)}
-    </>
-  );
-  */
-
-  function getHeaderText() {
-    if (active) {
-      if (hasId) {
-        return 'Your Meta ID'
-      }
-      else {
-        return 'You need a Meta ID'
-      }
-    } else {
-      return 'Join us in the Metaverse!'
-    }
-  }
-
-  function getSvg() {
-    if (active) {
-      return <MetaId data={db} />
-    } else {
-      return <Image src={exampleGif} alt="meta id examples" />
-    }
-  }
-
-  function getAction() {
-    if (active) {
-      if (hasId) {
-        return `${account}`
-      }
-      else {
-        if (isEligible) {
-          return 'Mint Meta ID'
-        } else {
-          return 'Mint a Character first!'
-        }
-      }
-    } else {
-      return <Connect />
-    }
-  }
 
   return (
     <>
@@ -153,7 +21,7 @@ function Home() {
           </h1>
         </div>
         <div>
-          {getSvg()}
+          <Image src={exampleGif} alt="meta id examples" />
         </div>
         <div>
           {getAction()}
--- pages/mint.js
+++ pages/mint.js
@@ -1,4 +1,119 @@
+import Image from 'next/image'
+import { Connect, MetaId } from '../components'
+
 function Mint() {
+  const { active, account } = useWeb3React()
+
+  const db = {
+    identity: {
+      name: null,
+      pfp: {
+        contract: null,
+        id: null,
+        image: null,
+        race: null,
+        role: null,
+        element: null
+      },
+      character: {
+        id: null,
+        race: null,
+        role: null,
+        element: null
+      },
+    },
+    equipment: {
+      weapon: null,
+      chestArmor: null,
+      headArmor: null,
+      waistArmor: null,
+      footArmor: null,
+      handAmor: null,
+      necklace: null,
+      ring: null
+    },
+    baseStats: {
+      str: null,
+      dex: null,
+      con: null,
+      int: null,
+      wis: null,
+      cha: null,
+    },
+    bonusStats: {
+      str: null,
+      dex: null,
+      con: null,
+      int: null,
+      wis: null,
+      cha: null,
+    }
+  }
+
+  /*
+  const [data, setData] = useState(null)
+  const [isLoading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setLoading(true)
+    fetch(`api/v1/stats/${account}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data)
+        setLoading(false)
+      })
+  }, [account])
+
+  if (isLoading) return <p>Loading...</p>
+
+  const dataComponent = (
+    <>
+      {isLoading ? <p>Loading...</p> : readStats(data)}
+    </>
+  );
+  */
+
+  const [hasId, setHasId] = useState(false)
+  const [isEligible, setIsEligible] = useState(false)
+
+  function getHeaderText() {
+    if (active) {
+      if (hasId) {
+        return 'Your Meta ID'
+      }
+      else {
+        return 'You need a Meta ID'
+      }
+    } else {
+      return 'Join us in the Metaverse!'
+    }
+  }
+
+  function getSvg() {
+    if (active) {
+      return <MetaId data={db} />
+    } else {
+      return <Image src={exampleGif} alt="meta id examples" />
+    }
+  }
+
+  function getAction() {
+    if (active) {
+      if (hasId) {
+        return `${account}`
+      }
+      else {
+        if (isEligible) {
+          return 'Mint Meta ID'
+        } else {
+          return 'Mint a Character first!'
+        }
+      }
+    } else {
+      return <Connect />
+    }
+  }
+
   return (
     <div></div>
   )
