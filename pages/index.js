485506784bc6b8fec4c59db44ca463a35ebe641f
michaelcjoseph <michael.joseph@hey.com>
2022-06-13 22:50:54 -0500

    Fix bug with populating Meta ID attributes

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,58 @@
 import { useMediaQuery } from 'react-responsive'
 import Head from 'next/head'
 import Image from 'next/image'
-import { Button, Card } from '../components'
+import { Button, Card, MetaId } from '../components'
 import exampleGif from '../public/metaid-examples-light.gif'
 import styles from '../styles/pages/index.module.css'
 
 function Home() {
   const isMobile = useMediaQuery({ maxWidth: 480 })
+
+  const exampleData = {
+    identity: {
+      name: 'michaelcjoseph.eth',
+      pfp: {
+        contract: '0x439cac149B935AE1D726569800972E1669d17094',
+        id: 9392,
+        image: 'https://lh3.googleusercontent.com/K3Bpl07pp03XcbkBiAGgpZbJpS1tsp6bd6OadaDJw_ZV2xM-H25W75B-N4LWv5bGsNCyjalLiPLqzjqnVefAiJI9D2zkeJf6wp7rDIA=w600',
+        race: 'Human',
+        role: null,
+        element: null
+      },
+      character: {
+        id: 1,
+        race: 'Gnome',
+        role: 'Chef',
+        element: 'Wind'
+      },
+    },
+    equipment: {
+      weapon: '"Dusk Whisper" Ghost Wand of Titans',
+      chestArmor: 'Plate Mail of Rage',
+      headArmor: '"Skull Moon" Divine Hood of Skill +1',
+      waistArmor: 'Mesh Belt of Protection',
+      footArmor: 'Demonhide Boots',
+      handAmor: 'Demon\'s Hands of Vitriol',
+      necklace: 'Pendant',
+      ring: 'Gold Ring'
+    },
+    baseStats: {
+      str: 24,
+      dex: 17,
+      con: 36,
+      int: 32,
+      wis: 41,
+      cha: 21,
+    },
+    bonusStats: {
+      str: 8,
+      dex: 6,
+      con: 6,
+      int: 2,
+      wis: 10,
+      cha: 2,
+    }
+  }
 
   return (
     <>
@@ -42,13 +88,21 @@ function Home() {
           </div>
         </div>
         <Card>
-          Test Card 1
+          <div>
+            <MetaId data={exampleData} />
+          </div>
         </Card>
         <Card tanBackground>
           Test Card 2
         </Card>
         <Card>
           Test Card 3
+        </Card>
+        <Card tanBackground>
+          Test Card 4
+        </Card>
+        <Card>
+          Test Card 5
         </Card>
       </div>
     </>
--- util/identity.js
+++ util/identity.js
@@ -14,26 +14,38 @@ function getId(identity) {
 
 function getRace(identity) {
   if (identity.character.race) {
-    return `${identity.pfp.race} + ${identity.character.race}`
+    if (identity.pfp.race) {
+      return `${identity.pfp.race} + ${identity.character.race}`
+    }
+
+    return identity.character.race
   }
   
-  return identity.pfp.race
+  return identity.pfp.race ? identity.pfp.race : 'None'
 }
 
 function getRole(identity) {
   if (identity.character.role) {
-    return `${identity.pfp.role} + ${identity.character.role}`
+    if (identity.pfp.role) {
+      return `${identity.pfp.role} + ${identity.character.role}`
+    }
+
+    return identity.character.role
   }
   
-  return identity.pfp.role
+  return identity.pfp.role ? identity.pfp.role : 'None'
 }
 
 function getElement(identity) {
   if (identity.character.element) {
-    return `${identity.pfp.element} + ${identity.character.element}`
+    if (identity.pfp.element) {
+      return `${identity.pfp.element} + ${identity.character.element}`
+    }
+
+    return identity.character.element
   }
   
-  return identity.pfp.element
+  return identity.pfp.element ? identity.pfp.element : 'None'
 }
 
 export {
