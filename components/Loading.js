4a3bfce3d49d428f68b251841692a073a10e9189
michaelcjoseph <michael.joseph@hey.com>
2022-07-30 23:45:39 -0500

    Set up Loading component

---
+++ components/Loading.js
@@ -0,0 +1,11 @@
+import styles from '../styles/components/Loading.module.css';
+
+function Loading() {
+  return (
+    <div>
+      Loading...
+    </div>
+  )
+}
+
+export default Loading
--- components/index.js
+++ components/index.js
@@ -2,6 +2,7 @@ import Button from './Button.js';
 import Card from './Card.js';
 import Footer from './Footer.js';
 import Header from './Header.js';
+import Loading from './Loading.js';
 import MetaId from './MetaId.js';
 import Mint from './Mint.js';
 import Page from './Page.js';
@@ -10,10 +11,11 @@ import Withdraw from './Withdraw.js';
 export {
   Button,
   Card,
+  Footer,
   Header,
-  Page,
+  Loading,
   MetaId,
   Mint,
-  Footer,
+  Page,
   Withdraw
 };
