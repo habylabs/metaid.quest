f967dba7df48e369abf8dd53cf7538c10f71aec8
michaelcjoseph <michael.joseph@hey.com>
2022-10-18 18:33:28 -0500

    Return svg string for tokenURI image instead of base64

--- util/image.js
+++ util/image.js
@@ -1,7 +1,7 @@
-function getContractImage(identity, stats) {
+const getContractImage = (identity, stats) => {
   // This should return a Base64 encoded string where the start of the string is `data:image/svg+xml;base64,`
   const svg = `
-    <svg width="550" height="500" viewBox="0 0 550 500" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width='550' height='500' viewBox='0 0 550 500' fill='none' xmlns='http://www.w3.org/2000/svg'>
       <style>
         .level-title { fill: #BBB; font-family: monospace; font-size: 18px; }
         .level-num { fill: #FFF; font-family: monospace; font-size: 60px; }
@@ -10,19 +10,21 @@ function getContractImage(identity, stats) {
           stroke-width: 2px;
         }
       </style>
-      <rect width="550" height="500" fill="#002B36"/>
-      <image href="${identity.pfp.image}" height="428" width="428" x="41" y="36"/>
-      <rect x="41" y="36" width="428" height="428" stroke="#002B36" stroke-width="30"/>
-      <rect x="41" y="36" width="428" height="428" class="rect-border"/>
-      <rect x="46" y="41" width="418" height="418" class="rect-border"/>
-      <path d="M433.077 305.292L401.155 250L433.077 194.708H496.923L528.845 250L496.923 305.292H433.077Z" fill="#002B36" class="rect-border"/>
-      <text x="450" y="225" class="level-title">LVL</text>
-      <text x="430" y="280" class="level-num">${stats.level}</text>
+      <rect width='550' height='500' fill='#002B36'/>
+      <image href='${identity.pfp.image}' height='428' width='428' x='41' y='36'/>
+      <rect x='41' y='36' width='428' height='428' stroke='#002B36' stroke-width='30'/>
+      <rect x='41' y='36' width='428' height='428' class='rect-border'/>
+      <rect x='46' y='41' width='418' height='418' class='rect-border'/>
+      <path d='M433.077 305.292L401.155 250L433.077 194.708H496.923L528.845 250L496.923 305.292H433.077Z' fill='#002B36' class='rect-border'/>
+      <text x='450' y='225' class='level-title'>LVL</text>
+      <text x='430' y='280' class='level-num'>${stats.level}</text>
     </svg>
   `
 
-  const bufferObj = Buffer.from(svg, "utf8")
-  return `data:image/svg+xml;base64,${bufferObj.toString("base64")}`
+  return svg
+
+  //const bufferObj = Buffer.from(svg).toString('base64')
+  //return `data:image/svg+xml;base64,${bufferObj}`
 }
 
 export {
